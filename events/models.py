22dcc8ba65c2bc4a73f5d395329a39e1ba68a76b
Alvin <alvinksabu200115@gmail.com>
2024-09-19 11:27:34 +0530

    Add initial project files

--- events/models.py
+++ events/models.py
@@ -207,12 +207,13 @@ class CollegePoster(models.Model):
 class NewsItem(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to='news_images/', blank=True, null=True)
+    image = models.ImageField(upload_to='news_images/', blank=True, null=True)  # Allows image to be optional
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
     
 
 class EventPhoto(models.Model):
