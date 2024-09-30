a56cf5711cbefb6a2b548f20585169a3c8a2f13f
aleeshaps123 <aleeshasibi26@gmail.com>
2024-09-30 13:52:51 +0530

    latest

--- events/urls.py
+++ events/urls.py
@@ -32,6 +32,7 @@ from .views import (
 )
 urlpatterns = [
     # Existing paths
+    
     path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
